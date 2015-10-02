3e90c684c5d20674893f20708f3e1705c64ad46f
Charlie Hebert <charlie.hebert92@gmail.com>
2015-10-02 15:17:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+CS4052 Computer Graphics
+========================
+A repo for all the work I do on this module at Trinity College Dublin.
+
+## Useful Commands
+Compile the source code into `myProgram`:
+
+    g++ -o myProgram main.cpp -lglfw -framework OpenGl -lGLEW
---
+++ glfw-3.1.1/build/src/cmake_install.cmake
@@ -0,0 +1,52 @@
+# Install script for directory: /Users/chooie/code/computer_graphics/glfw-3.1.1/src
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE SHARED_LIBRARY FILES
+    "/Users/chooie/code/computer_graphics/glfw-3.1.1/build/src/libglfw.3.1.dylib"
+    "/Users/chooie/code/computer_graphics/glfw-3.1.1/build/src/libglfw.3.dylib"
+    "/Users/chooie/code/computer_graphics/glfw-3.1.1/build/src/libglfw.dylib"
+    )
+  foreach(file
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libglfw.3.1.dylib"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libglfw.3.dylib"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libglfw.dylib"
+      )
+    if(EXISTS "${file}" AND
+       NOT IS_SYMLINK "${file}")
+      execute_process(COMMAND "/usr/bin/install_name_tool"
+        -id "/usr/local/lib/libglfw.3.dylib"
+        "${file}")
+      if(CMAKE_INSTALL_DO_STRIP)
+        execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "${file}")
+      endif()
+    endif()
+  endforeach()
+endif()
+
